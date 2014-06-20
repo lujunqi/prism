c211748f8b7de74a3e650edb8fe54bbc36827707
lujunqi <19995945@qq.com>
2014-06-20 10:16:58 +0800

    ok

--- src/com/prism/exception/BMOException.java
+++
@@ -1,8 +0,0 @@
-package com.prism.exception;
-
-public class BMOException extends Exception {
-	private static final long serialVersionUID = 1L;
-	public BMOException(Exception e){
-		super(e);
-	}
-}
--- src/com/prism/exception/DAOException.java
+++
@@ -1,8 +0,0 @@
-package com.prism.exception;
-
-public class DAOException extends Exception {
-	private static final long serialVersionUID = 1L;
-	public DAOException(Exception e){
-		super(e);
-	}
-}
